346281b5e8d5298724ea217771d37a0a57857c1a
Desk Fran <lugojuanfracisco@gmail.com>
2021-03-21 23:14:05 -0300

    clear + - x / funcionan en vista

--- view.js
+++ view.js
@@ -1,50 +1,46 @@
 export default class View {
     constructor() {
         this.calculadora = document.getElementById("section-calculadora");
-        this.inputNow = document.getElementById('input-now');
+        this.inputNow = document.getElementById('now');
     }
 
     createButons() {
 
-        const butons = document.getElementById("calc-buttons");
-        // butons.classList.add("columns", "is-centered");
+        const btn = document.getElementById("suma");
 
-        const sumaBtn = document.createElement("button");
-        sumaBtn.classList.add("button", "is-primary", "is-outlined");
-        sumaBtn.innerHTML = "+";
-        butons.appendChild(sumaBtn);
-        sumaBtn.onclick = () => {this.renderInputs('+')};
+        // const butons = document.getElementById("calc-buttons");
 
-        const restaBtn = document.createElement("button");
-        restaBtn.classList.add("button", "is-primary", "is-outlined");
-        restaBtn.innerHTML = "-";
-        butons.appendChild(restaBtn);
+        // const sumaBtn = document.createElement("button");
+        // sumaBtn.classList.add("button", "is-primary", "is-outlined");
+        // sumaBtn.innerHTML = "+";
+        // butons.appendChild(sumaBtn);
+        // sumaBtn.onclick = () => this.setImputNow('+');
 
-        const mulBtn = document.createElement("button");
-        mulBtn.classList.add("button", "is-primary", "is-outlined");
-        mulBtn.innerHTML = "x";
-        butons.appendChild(mulBtn);
+        // const restaBtn = document.createElement("button");
+        // restaBtn.classList.add("button", "is-primary", "is-outlined");
+        // restaBtn.innerHTML = "-";
+        // butons.appendChild(restaBtn);
 
-        const divBtn = document.createElement("button");
-        divBtn.classList.add("button", "is-primary", "is-outlined");
-        divBtn.innerHTML = "/";
-        butons.appendChild(divBtn);
+        // const mulBtn = document.createElement("button");
+        // mulBtn.classList.add("button", "is-primary", "is-outlined");
+        // mulBtn.innerHTML = "x";
+        // butons.appendChild(mulBtn);
+
+        // const divBtn = document.createElement("button");
+        // divBtn.classList.add("button", "is-primary", "is-outlined");
+        // divBtn.innerHTML = "/";
+        // butons.appendChild(divBtn);
         
-        const resultBtn = document.createElement("button");
-        resultBtn.classList.add("button", "is-info", "is-outlined");
-        resultBtn.innerHTML = "=";
-        butons.appendChild(resultBtn);
+        // const resultBtn = document.createElement("button");
+        // resultBtn.classList.add("button", "is-info", "is-outlined");
+        // resultBtn.innerHTML = "=";
+        // butons.appendChild(resultBtn);
         
-
-        
-        
-
 
     }
 
-    renderInputs(op){
-        console.log("llegue");
-        // const inputNow = document.getElementsById('input-now');
-        // this.inputNow.placeholder = '+';
+    setImputNow(op){
+        document.getElementById("demo").innerHTML += op;
     }
+
 }
